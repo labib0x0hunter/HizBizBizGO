3b0ea26ac5b183d3e63d608e573de082f24856ea
Labib Al Faisal <89539780+labib0x0hunter@users.noreply.github.com>
2025-04-10 18:28:18 +0600

    Update echoServer.go

--- echoServer.go
+++ echoServer.go
@@ -3,41 +3,75 @@ package main
 import(
     "fmt"
     "net"
-    "strings"
 )
 
-func handleConn(conn net.Conn) {
+// Server Configuration
+type Config struct {
+    address string
+    port    string
+}
+
+// Server
+type Server struct {
+    listener net.Listener
+    cfg     Config
+}
+
+// Starts the server
+func (s *Server) Start() {
+    addr := net.JoinHostPort(s.cfg.address, s.cfg.port)
+    fmt.Println(addr)
+    var err error
+    s.listener, err = net.Listen("tcp", addr)
+    if err != nil {
+        fmt.Println("Error in Listening")
+        return
+    }
+
+    defer s.listener.Close()
+    fmt.Println("Server is listening at ", addr)
+
+    // Handle connection
+    for{
+        conn, err := s.listener.Accept()
+        if err != nil {
+            fmt.Println("Error in Incoming request")
+            continue
+        }
+
+        // Start Goroutine
+        go s.HandleConnection(conn)
+    }
+}
+
+// Handle Connection for each connection
+func (s *Server) HandleConnection(conn net.Conn) {
     defer conn.Close()
-    conn.Write([]byte("Welcome to HizBizBiz\n"))
+    conn.Write([]byte("Welcome to Server\n"))
+
     buffer := make([]byte, 1024)
     for{
-        conn.Write([]byte(" >>> "))
+        conn.Write([]byte(" -> "))
         n, err := conn.Read(buffer)
         if err != nil {
-            fmt.Println("Client disconnected")
+            fmt.Println("Client Disconnected\n")
             return
         }
-        input := strings.TrimSpace(string(buffer[:n]))
-        response := fmt.Sprintf("%s\n", input)
+        input := buffer[:n]
+        response := string(input)
         conn.Write([]byte(response))
     }
 }
 
 func main() {
-        
-    listener, err := net.Listen("tcp", ":8080")
-    if err != nil{
-        panic("Error - 01")
+    cfg := Config{
+        address : "127.0.0.1",
+        port :  "8080",
     }
-    defer listener.Close()
 
-    // Handle multiple client
-    for {
-        conn, err := listener.Accept()
-        if err != nil{
-            fmt.Println("Error accepting client : ", err)
-            continue
-        }
-        go handleConn(conn)
+    server := Server{
+        cfg : cfg,
     }
+
+    server.Start()
 }
